6287e16108917aa2e558b907a116a72bde4ff419
CruzeFW <Cruzee@gmx.at>
2024-10-16 12:09:55 +0200

    initial commit

---
+++ src/main/java/fhtechnikum/at/swkom_paperless_groupf/SwkomPaperlessGroupFApplication.java
@@ -0,0 +1,13 @@
+package fhtechnikum.at.swkom_paperless_groupf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwkomPaperlessGroupFApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwkomPaperlessGroupFApplication.class, args);
+    }
+
+}
