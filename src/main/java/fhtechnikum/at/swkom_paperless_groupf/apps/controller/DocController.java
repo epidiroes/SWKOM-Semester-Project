2862fd5567ceb35e07f4754cb2ec54013c44b74d
laura <laura.engleitner@gmail.com>
2024-12-04 20:49:37 +0100

    add logging for DocController, DocService and Doc

--- src/main/java/fhtechnikum/at/swkom_paperless_groupf/apps/controller/DocController.java
+++ src/main/java/fhtechnikum/at/swkom_paperless_groupf/apps/controller/DocController.java
@@ -2,6 +2,7 @@ package fhtechnikum.at.swkom_paperless_groupf.apps.controller;
 
 import fhtechnikum.at.swkom_paperless_groupf.apps.entity.Doc;
 import fhtechnikum.at.swkom_paperless_groupf.apps.service.DocService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,25 +15,37 @@ import java.util.List;
 @RestController
 @RequestMapping("/docs")
 @CrossOrigin
-
+@Slf4j
 public class DocController {
     @Autowired
     DocService docService;
 
     @GetMapping
     public ResponseEntity<List<Doc>> getDocs(){
-
-        return new ResponseEntity<>(docService.findAll(), HttpStatus.OK);
+        log.info("Fetching all documents...");
+        List<Doc> docs = docService.findAll();
+        log.debug("Fetched {} documents.", docs.size());
+        return new ResponseEntity<>(docs, HttpStatus.OK);
     }
 
     @PostMapping("/upload")
     public ResponseEntity<Doc> uploadDoc(@RequestParam("file") MultipartFile file) throws IOException {
+        log.info("Received a file upload request: {}", file.getOriginalFilename());
+
         if(file.isEmpty()){
+            log.warn("File upload failed: Empty file.");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-        // TODO: fix the return type, weil wtf soll das sein
-        return new ResponseEntity<>(docService.saveDocument(file), HttpStatus.OK);
+        // old note from before add logging: fix the return type, weil wtf soll das sein
+        try {
+            Doc savedDoc = docService.saveDocument(file);
+            log.info("File successfully saved: {}", savedDoc.getId());
+            return new ResponseEntity<>(savedDoc, HttpStatus.OK);
+        } catch (IOException e) {
+            log.error("Error while saving file: {}", file.getOriginalFilename(), e);
+            throw e;
+        }
     }
 
 }
--- src/main/java/fhtechnikum/at/swkom_paperless_groupf/apps/service/DocService.java
+++ src/main/java/fhtechnikum/at/swkom_paperless_groupf/apps/service/DocService.java
@@ -2,6 +2,7 @@ package fhtechnikum.at.swkom_paperless_groupf.apps.service;
 
 import fhtechnikum.at.swkom_paperless_groupf.apps.entity.Doc;
 import fhtechnikum.at.swkom_paperless_groupf.apps.repository.DocRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,21 +11,32 @@ import java.io.IOException;
 import java.util.List;
 
 @Service
+@Slf4j
 public class DocService {
 
     @Autowired
     private DocRepository docRepository;
 
     public Doc saveDocument(MultipartFile fileToSave) throws IOException {
-        Doc doc = new Doc(fileToSave.getOriginalFilename(), fileToSave.getBytes());
-        Doc savedDoc = docRepository.save(doc);
+        log.info("Saving document: {}", fileToSave.getOriginalFilename());
 
-        // TODO: fix the return type, weil wtf soll das sein
-        return savedDoc;
+        // old note before add logging: fix the return type, weil wtf soll das sein
+        try {
+            Doc doc = new Doc(fileToSave.getOriginalFilename(), fileToSave.getBytes());
+            Doc savedDoc = docRepository.save(doc);
+            log.info("Document saved successfully with ID: {}", savedDoc.getId());
+            return savedDoc;
+        } catch (IOException e) {
+            log.error("Error saving document: {}", fileToSave.getOriginalFilename(), e);
+            throw e;
+        }
     }
 
     public List<Doc> findAll() {
-        return docRepository.findAll();
+        log.info("Fetching all documents...");
+        List<Doc> docs = docRepository.findAll();
+        log.debug("Fetched {} documents from the repository.", docs.size());
+        return docs;
     }
 
 }
