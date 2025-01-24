6e1375e36966abba0eaed6037d79013a29acc972
CruzeFW <Cruzee@gmx.at>
2025-01-24 23:12:55 +0100

    refactored into two modules

---
+++ OCRWorker/src/main/java/fhtechnikum/at/ocrworker/OcrWorkerApplication.java
@@ -0,0 +1,13 @@
+package fhtechnikum.at.ocrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcrWorkerApplication.class, args);
+    }
+
+}
---
+++ OCRWorker/src/test/java/fhtechnikum/at/ocrworker/OcrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtechnikum.at.ocrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
